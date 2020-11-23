8ce33b34098a350ce127439586b528cc2eac1de1
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2020-11-23 17:58:43 -0300

    serializer

--- tasks/models.py
+++ tasks/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
     description = models.CharField(max_length=500)
