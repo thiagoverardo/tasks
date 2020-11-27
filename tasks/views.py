db7a34a3403041992771718b3bf0c19c95aa0ad5
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2020-11-27 11:18:06 -0300

    fixed

--- tasks/views.py
+++ tasks/views.py
@@ -35,6 +35,5 @@ def post_task(request):
 
 @api_view(["DELETE"])
 def delete_tasks(request):
-    if()
     tasks = Task.objects.all().delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
